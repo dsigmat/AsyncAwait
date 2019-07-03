e56408da147499d21db44e9696d2f985dfbb9f49
Denis Sidorov <dsigmat@hotmail.com>
2019-07-03 12:14:52 +0600

    async поток

--- AsyncAwait/Program.cs
+++ AsyncAwait/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace AsyncAwait
 {
@@ -6,7 +7,52 @@ namespace AsyncAwait
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Thread thread = new Thread(new ThreadStart(DoWork));
+            thread.Start();
+
+            Thread thread2 = new Thread(new ParameterizedThreadStart(DoWork));
+            thread2.Start(int.MaxValue);
+
+            int j = 0;
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                j++;
+
+                if (j % 10000 == 0)
+                {
+                    Console.WriteLine("Main DoWork 1");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        static void DoWork()
+        {
+            int j = 0;
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                j++;
+
+                if (j%10000 == 0)
+                {
+                    Console.WriteLine("Potok DoWork 2");
+                }
+            }
+        }
+
+        static void DoWork(object max)
+        {
+            int j = 0;
+            for (int i = 0; i < (int)max; i++)
+            {
+                j++;
+
+                if (j % 10000 == 0)
+                {
+                    Console.WriteLine("Potok DoWork 3");
+                }
+            }
         }
     }
 }
