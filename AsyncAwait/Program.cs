a864f1cb93148ebfe4fdbc0cc166e8081724a859
Denis Sidorov <dsigmat@hotmail.com>
2019-07-03 14:55:55 +0600

    потоки

--- AsyncAwait/Program.cs
+++ AsyncAwait/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsyncAwait
 {
@@ -7,37 +10,78 @@ namespace AsyncAwait
     {
         static void Main(string[] args)
         {
-            Thread thread = new Thread(new ThreadStart(DoWork));
-            thread.Start();
+            #region thread
+            //Thread thread = new Thread(new ThreadStart(DoWork));
+            //thread.Start();
 
-            Thread thread2 = new Thread(new ParameterizedThreadStart(DoWork));
-            thread2.Start(int.MaxValue);
+            //Thread thread2 = new Thread(new ParameterizedThreadStart(DoWork));
+            //thread2.Start(int.MaxValue);
 
-            int j = 0;
-            for (int i = 0; i < int.MaxValue; i++)
-            {
-                j++;
+            //int j = 0;
+            //for (int i = 0; i < int.MaxValue; i++)
+            //{
+            //    j++;
 
-                if (j % 10000 == 0)
-                {
-                    Console.WriteLine("Main DoWork 1");
-                }
-            }
+            //    if (j % 10000 == 0)
+            //    {
+            //        Console.WriteLine("Main DoWork 1");
+            //    }
+            //}
+            #endregion
 
-            Console.ReadLine();
+            #region async/await
+            //Console.WriteLine("Begin main");
+
+            //DoWorkAsync(1000);
+
+            //Console.WriteLine("Continue Main");
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //        Console.WriteLine("Main");
+            //}
+            //Console.WriteLine("End Main");
+            #endregion
+
+            var result = SaveFileAsync("test.txt");
+            var input = Console.ReadLine();
+            Console.WriteLine(result.Result);
         }
 
-        static void DoWork()
+        static async Task<bool> SaveFileAsync(string path)
         {
-            int j = 0;
-            for (int i = 0; i < int.MaxValue; i++)
-            {
-                j++;
+            var result = await Task.Run(() => SaveFile(path));
+            return result;
+        }
 
-                if (j%10000 == 0)
-                {
+        static bool SaveFile(string path)
+        {
+            var rnd = new Random();
+            var txt = "";
+            for (int i = 0; i < 100; i++)
+            {
+                txt += rnd.Next();
+            }
+
+            using (var sw = new StreamWriter(path, false, Encoding.UTF8))
+            {
+                sw.WriteLine(txt);
+            }
+            return true;
+        }
+
+        static async Task DoWorkAsync(int max)
+        {
+            Console.WriteLine("Begin Async");
+            await Task.Run(() => DoWork(max));
+            Console.WriteLine("End Async");
+        }
+
+        static void DoWork(int max)
+        {
+            for (int i = 0; i < max; i++)
+            {
                     Console.WriteLine("Potok DoWork 2");
-                }
             }
         }
 
